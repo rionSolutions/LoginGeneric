bdaff5b1626a34ca92df3a8111fccd636b295f82
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2024-09-10 00:34:29 -0300

    ADD DOCS

--- src/main/java/com/orionsolution/auth/controller/LoginController.java
+++ src/main/java/com/orionsolution/auth/controller/LoginController.java
@@ -3,6 +3,11 @@ package com.orionsolution.auth.controller;
 import com.orionsolution.auth.model.CredentialsDTO;
 import com.orionsolution.auth.model.ResourcesPolicyDataDTO;
 import com.orionsolution.auth.service.AuthenticatorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/login")
@@ -19,15 +23,33 @@ public class LoginController {
 
     private final AuthenticatorService authenticatorService;
 
+    /**
+     * Constructor for LoginController.
+     *
+     * @param authenticatorService the service for handling authentication logic
+     */
     public LoginController(AuthenticatorService authenticatorService) {
         this.authenticatorService = authenticatorService;
     }
 
+    /**
+     * Handles the login request.
+     *
+     * @param credentials a map containing the username and password
+     * @return ResponseEntity containing ResourcesPolicyDataDTO and HTTP status
+     */
+    @Operation(summary = "Handles the login request")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful login",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResourcesPolicyDataDTO.class))}),
+            @ApiResponse(responseCode = "401", description = "Invalid credentials",
+                    content = @Content)
+    })
     @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<ResourcesPolicyDataDTO> login(@RequestParam Map<String, String> credentials) {
-        authenticatorService.authenticate(new CredentialsDTO(credentials.get("username"), credentials.get("password")));
-        return  new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(authenticatorService.authenticate(
+                new CredentialsDTO(credentials.get("username"), credentials.get("password"))),
+                HttpStatus.OK);
     }
-
-
 }
--- src/main/java/com/orionsolution/auth/service/impl/AuthenticatorServiceImpl.java
+++ src/main/java/com/orionsolution/auth/service/impl/AuthenticatorServiceImpl.java
@@ -12,22 +12,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service implementation for handling authentication logic.
+ */
 @Service
 public class AuthenticatorServiceImpl implements AuthenticatorService {
 
     private final UserRepository userRepository;
 
+    /**
+     * Constructor for AuthenticatorServiceImpl.
+     *
+     * @param userRepository the repository for accessing user data
+     */
     public AuthenticatorServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Authenticates the user based on provided credentials.
+     *
+     * @param credentials the credentials provided by the user
+     * @return ResourcesPolicyDataDTO containing user information and authorization data
+     * @throws BusinessException.HandlerException if the credentials are invalid or the email is not validated
+     */
     @Override
     public ResourcesPolicyDataDTO authenticate(CredentialsDTO credentials) {
         User user = userRepository.findByEmail(credentials.username());
         if (user != null && user.isValidate()) {
             BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
             if (bCryptPasswordEncoder.matches(credentials.password(), user.getPassword())) {
-                return new ResourcesPolicyDataDTO(user.getUsername(), user.getSurname(), user.getEmail(), user.getPhone(), user.getDocument(), new AuthorizationDTO(ApplicationKeyUtility.getAuthorization()));
+                return new ResourcesPolicyDataDTO(user.getName(), user.getSurname(), user.getEmail(), user.getPhone(), user.getDocument(), new AuthorizationDTO(ApplicationKeyUtility.getAuthorization()));
             }
         }
         throw new BusinessException.HandlerException("Invalid credentials or Email not validated", HttpStatus.UNAUTHORIZED);
